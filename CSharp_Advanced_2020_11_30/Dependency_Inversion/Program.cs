6d0107fd2ab63ce78f5a9a6986484da210441359
Kevin Winter <KevinW@ppedv.de>
2020-12-01 10:20:12 +0100

    Solid Part2

---
+++ CSharp_Advanced_2020_11_30/Dependency_Inversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependency_Inversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
